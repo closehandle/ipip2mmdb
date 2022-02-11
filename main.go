5aa4a092bd453ead4daee4d56743bc7b6c7267fa
CloseHandle <88403145+closehandle@users.noreply.github.com>
2022-02-12 03:35:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,66 +2,16 @@ package main
 
 import (
 	"bufio"
-	"flag"
+	"log"
+	"net"
+	"os"
+
 	"github.com/maxmind/mmdbwriter"
 	"github.com/maxmind/mmdbwriter/mmdbtype"
-	log "github.com/sirupsen/logrus"
-	"os"
 )
-		//"country": mmdbtype.Map{
-		//"continent": mmdbtype.Map{
-		//"code": mmdbtype.String("AS"),
-		//"geoname_id": mmdbtype.Uint32(6255147),
-		//"names":mmdbtype.Map{"de":mmdbtype.String("Asien"),
-		//"en":mmdbtype.String("Asia"),
-		//"es":mmdbtype.String("Asia"),
-		//"fr":mmdbtype.String("Asie"),
-		//"ja":mmdbtype.String("アジア"),
-		//"pt-BR":mmdbtype.String("Ásia"),
-		//"ru":mmdbtype.String("Азия"),
-		//"zh-CN":mmdbtype.String("亚洲")},
-		//},
-		//"country": mmdbtype.Map{
-		//"geoname_id":mmdbtype.Uint32(1814991),
-		//"is_in_european_union":mmdbtype.Bool(false),
-		//"iso_code":mmdbtype.String("CN"),
-		//"names":mmdbtype.Map{
-		//"de":mmdbtype.String("China"),
-		//"en":mmdbtype.String("China"),
-		//"es":mmdbtype.String("China"),
-		//"fr":mmdbtype.String("Chine"),
-		//"ja":mmdbtype.String("中国"),
-		//"pt-BR":mmdbtype.String("China"),
-		//"ru":mmdbtype.String("Китай"),
-		//"zh-CN":mmdbtype.String("中国"),
-		//},
-		//},
-		//"registered_country": mmdbtype.Map{
-		//"geoname_id":mmdbtype.Uint32(1814991),
-		//"is_in_european_union":mmdbtype.Bool(false),
-		//"iso_code":mmdbtype.String("CN"),
-		//"names":mmdbtype.Map{
-		//"de":mmdbtype.String("China"),
-		//"en":mmdbtype.String("China"),
-		//"es":mmdbtype.String("China"),
-		//"fr":mmdbtype.String("Chine"),
-		//"ja":mmdbtype.String("中国"),
-		//"pt-BR":mmdbtype.String("China"),
-		//"ru":mmdbtype.String("Китай"),
-		//"zh-CN":mmdbtype.String("中国"),
-		//},
-		//},
-		//"traits": mmdbtype.Map{
-		//"is_anonymous_proxy": mmdbtype.Bool(false),
-		//"is_satellite_provider":mmdbtype.Bool(false),
-		//},
-		//},
 
 var (
-	srcFile string
-	dstFile string
-	databaseType string
-	cnRecord = mmdbtype.Map{
+	Record = mmdbtype.Map{
 		"country": mmdbtype.Map{
 			"geoname_id":           mmdbtype.Uint32(1814991),
 			"is_in_european_union": mmdbtype.Bool(false),
@@ -80,17 +30,10 @@ var (
 	}
 )
 
-func init()  {
-	flag.StringVar(&srcFile, "s", "ipip_cn.txt", "specify source ip list file")
-	flag.StringVar(&dstFile, "d", "Country.mmdb", "specify destination mmdb file")
-	flag.StringVar(&databaseType,"t", "GeoIP2-Country", "specify MaxMind database type")
-	flag.Parse()
-}
-
-func main()  {
-	writer, err := mmdbwriter.New(
+func main() {
+	mmdb, err := mmdbwriter.New(
 		mmdbwriter.Options{
-			DatabaseType: databaseType,
+			DatabaseType: "GeoIP2-Country",
 			RecordSize:   24,
 		},
 	)
@@ -98,36 +41,73 @@ func main()  {
 		log.Fatalf("fail to new writer %v\n", err)
 	}
 
-	var ipTxtList []string
-	fh, err := os.Open(srcFile)
-	if err != nil {
-		log.Fatalf("fail to open %s\n", err)
-	}
-	scanner := bufio.NewScanner(fh)
-	scanner.Split(bufio.ScanLines)
-
-	for scanner.Scan() {
-		ipTxtList = append(ipTxtList, scanner.Text())
-	}
-
-	ipList := parseCIDRs(ipTxtList)
-	for _, ip := range ipList {
-		err = writer.Insert(ip, cnRecord)
+	{
+		file, err := os.Open("ipv4.txt")
 		if err != nil {
-			log.Fatalf("fail to insert to writer %v\n", err)
+			log.Fatalf("fail to open %s\n", err)
+		}
+
+		data := make([]string, 0)
+		scan := bufio.NewScanner(file)
+		scan.Split(bufio.ScanLines)
+		for scan.Scan() {
+			data = append(data, scan.Text())
+		}
+
+		list := ParseCIDR(data)
+		for _, addr := range list {
+			err = mmdb.Insert(addr, Record)
+			if err != nil {
+				log.Fatalf("fail to insert to writer %v\n", err)
+			}
 		}
 	}
 
-	outFh, err := os.Create(dstFile)
+	{
+		file, err := os.Open("ipv6.txt")
+		if err != nil {
+			log.Fatalf("fail to open %s\n", err)
+		}
+
+		data := make([]string, 0)
+		scan := bufio.NewScanner(file)
+		scan.Split(bufio.ScanLines)
+		for scan.Scan() {
+			data = append(data, scan.Text())
+		}
+
+		list := ParseCIDR(data)
+		for _, addr := range list {
+			err = mmdb.Insert(addr, Record)
+			if err != nil {
+				log.Fatalf("fail to insert to writer %v\n", err)
+			}
+		}
+	}
+
+	file, err := os.Create("Country.mmdb")
 	if err != nil {
 		log.Fatalf("fail to create output file %v\n", err)
 	}
 
-	_, err = writer.WriteTo(outFh)
+	_, err = mmdb.WriteTo(file)
 	if err != nil {
 		log.Fatalf("fail to write to file %v\n", err)
 	}
-
 }
 
+func ParseCIDR(strs []string) []*net.IPNet {
+	list := make([]*net.IPNet, 0)
 
+	for _, cidr := range strs {
+		_, addr, err := net.ParseCIDR(cidr)
+		if err != nil || addr == nil {
+			log.Printf("%s fail to parse to CIDR\n", cidr)
+			continue
+		}
+
+		list = append(list, addr)
+	}
+
+	return list
+}
