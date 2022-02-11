f5445601d93dca359ff29c589620207f354f9813
CloseHandle <88403145+closehandle@users.noreply.github.com>
2022-02-12 03:55:57 +0800

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 {
-    wget -q -O ipv4.txt https://cdn.jsdelivr.net/gh/17mon/china_ip_list@master/china_ip_list.txt
+    wget -q -O ipv4.txt https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
 }
 
 {
