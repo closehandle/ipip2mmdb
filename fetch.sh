8402ba3e499996d93b180df4f11066c65d4460e2
CloseHandle <88403145+closehandle@users.noreply.github.com>
2022-12-20 23:31:17 +0800

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -6,19 +6,15 @@ git clone https://github.com/FvDxxx/pfxaggr aggr && cd aggr && make -j$(nproc) &
 rm -f ipv4.txt ipv6.txt
 
 wget -O misakaio_chnroutes4.txt https://cdn.jsdelivr.net/gh/misakaio/chnroutes2@master/chnroutes.txt
-wget -O gaoyifan_chnroutes4.txt https://cdn.jsdelivr.net/gh/gaoyifan/china-operator-ip@ip-lists/china.txt
 wget -O 17mon_chnroutes4.txt    https://cdn.jsdelivr.net/gh/17mon/china_ip_list@master/china_ip_list.txt
 cat misakaio_chnroutes4.txt >> ipv4.txt
-cat gaoyifan_chnroutes4.txt >> ipv4.txt
 cat 17mon_chnroutes4.txt    >> ipv4.txt
 ./pfxaggr < ipv4.txt > chnroutes4
 
 curl -fsSL http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest | grep CN | grep ipv6 | awk -F'|' '{printf("%s/%d\n", $4, $5)}' > apnic_chnroutes6.txt
 wget -O clang_chnroutes6.txt    https://ispip.clang.cn/all_cn_ipv6.txt
-wget -O gaoyifan_chnroutes6.txt https://cdn.jsdelivr.net/gh/gaoyifan/china-operator-ip@ip-lists/china6.txt
 cat apnic_chnroutes6.txt    >> ipv6.txt
 cat clang_chnroutes6.txt    >> ipv6.txt
-cat gaoyifan_chnroutes6.txt >> ipv6.txt
 ./pfxaggr < ipv6.txt > chnroutes6
 
 echo 'ip firewall address-list remove [find list=China]' > chnroutes4.rsc
